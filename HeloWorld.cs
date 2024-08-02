af010fb1d3f38d007d5b6b3e8b5f6da0eb6081e3
Ptenchi <shnayderde@gmail.com>
2024-08-02 10:51:23 +0300

    load HeloWorld

---
+++ HeloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Helo, world");
+        //  выводит текст в кансоль
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
